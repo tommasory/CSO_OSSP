9f28b329892b49e5f1347f95511e4107220cfaef
Tommasory <tommasory@gmail.com>
2022-03-17 07:52:15 -0500

    Agrega lista de operaciones.

--- problems/ossp.py
+++ problems/ossp.py
@@ -1,6 +1,4 @@
 import numpy as np
-from pathlib import Path
-
 
 class ossp:
     size: int
@@ -10,8 +8,8 @@ class ossp:
         lines = file1.readlines()
 
         jobs  = int(lines[0])
-        self.work_list = []
-        flag = 1
+        self.operations = []
+        flag = 0
         for pos in range(1, jobs+1):
             str_line = lines[pos].replace('\n','')
             line = [int(e) for e in str_line.split(',')]
@@ -20,18 +18,20 @@ class ossp:
             for i in range(len(line)):
                 if i%2:
                     aux = [flag, job, line[i-1], line[i]]
-                    self.work_list.append(aux)
+                    self.operations.append(Operation(aux))
                     flag += 1
-        self.size = len(self.work_list)
+        self.size = len(self.operations)
 
     def evaluate(self, cells):
         fitness = 0
-        for i in range(self.size):
-            j = i + 1
-            if j >= self.size:
-                j = 0
-            fitness = fitness + self.distances[cells[i]][cells[j]]
-        return fitness
+        return np.random.randint(100)
 
-    def __str__(self):
-        return Path(self.file_name).stem
+class Operation:
+    def __init__(self, element):
+        self.number = element[0] 
+        self.job = element[1] 
+        self.machine = element[2] 
+        self.time = element[3] 
+
+    def __repr__(self):
+        return "<Operation number %s: job:%s, machine:%s, time:%s>" % (self.number, self.job, self.machine, self.time)
